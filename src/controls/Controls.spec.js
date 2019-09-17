fab0ffa203c7d3a384926d5c6631a759fdd004ba
Tyler Biswell <tsbiswell@gmail.com>
2019-09-17 10:58:00 -0600

    test controls component to provide buttons to toggle closed and locked states

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,10 @@
 // Test away!
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import Controls from './Controls';
+
+test('Controls Component provides buttons to toggle "closed" and "locked" states', () => {
+  const { getAllByRole } = render(<Controls />);
+  getAllByRole('button');
+});
