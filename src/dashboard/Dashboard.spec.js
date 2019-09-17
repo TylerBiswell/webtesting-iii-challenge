8e363ff968e327fa7a9085b7198ca6332bb0d2dc
Tyler Biswell <tsbiswell@gmail.com>
2019-09-17 10:57:10 -0600

    test dashboard to show controls and display

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,14 @@
 // Test away
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import Controls from '../controls/Controls';
+import Display from '../display/Display';
+
+test('Dashboard shows controls', () => {
+  render(<Controls />);
+});
+
+test('Dashboard shows display', () => {
+  render(<Display />);
+});
